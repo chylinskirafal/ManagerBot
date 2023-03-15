4769e9283425804eb71a508236d31a73bc9b8c7c
Chylu <rafal@chylu.pl>
2023-03-15 19:52:38 +0100

    First code bot

---
+++ src/main/java/pl/chylu/Main.java
@@ -0,0 +1,36 @@
+package pl.chylu;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+
+    private final Dotenv config;
+    private final ShardManager shardManager;
+
+    public Main() throws LoginException {
+        config = Dotenv.configure().load();
+
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(config.get("TOKEN"));
+        builder.setStatus(OnlineStatus.ONLINE);
+        builder.setActivity(Activity.playing("zgłoszenia wyCHYLenia"));
+        shardManager =builder.build();
+    }
+
+    public ShardManager getShardManager() {
+        return shardManager;
+    }
+
+    public static void main(String[] args) {
+        try {
+            Main bot = new Main();
+        } catch (LoginException e) {
+            System.out.println("Error: Proviceded bot token is invalid");
+        }
+    }
+}
